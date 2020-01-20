52f8edd7c1b33fc5262ecdb269eaf84b4e59765a
kamlesh.papnoi@reviver.com <Kcp@1993>
2020-01-20 16:11:05 +0530

    # Added path and test-data

---
+++ testdata.py
@@ -0,0 +1,9 @@
+class TestData:
+    # Login page data
+    Log_in_button_xpath = "//button[@data-id='BUTTON' and text()='Login']"
+    Email_box_xpath = "//input[@id ='1-email' and @placeholder = 'yours@example.com']"
+    Pwd_box_xpath = "//input[@name='password' and @placeholder = 'your password']"
+    log_in_xpath = "//button[@name = 'submit' and @aria-label = 'Log In']"
+
+    user_name = "ayush.andalpha@gmail.com"
+    password = "Tester@123"
